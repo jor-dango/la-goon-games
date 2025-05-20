5c2e8453898be33722b51ff246b7cbf3f96e156a
jor-dango <jtan208295@gmail.com>
2025-05-20 03:23:34 -0500

    Fix: Added Auth check for Form page

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -5,6 +5,7 @@ import { doc, DocumentData, getDoc, getFirestore, onSnapshot, setDoc, updateDoc 
 import { db, app } from '../firebase';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { ChevronDown } from 'lucide-react';
+import { AuthProvider } from '@/context/AuthProvider';
 
 type Challenge = {
   author: string;
@@ -109,59 +110,61 @@ function Form() {
   }
 
   return (
-    <div className="w-full h-[100vh] flex justify-center items-center">
-      <div className="max-w-[95%] flex flex-col gap-8 p-8 border border-border rounded-2xl bg-bglight">
-        {/* <button onClick={handleSignOut}>Log Out</button> */}
-        <div>
-          <small className='text-textsecondary'>Welcome {userInfo?.name}!</small>
-          <h3>Submit a Challenge</h3>
+    <AuthProvider>
+      <div className="w-full h-[100vh] flex justify-center items-center">
+        <div className="max-w-[95%] flex flex-col gap-8 p-8 border border-border rounded-2xl bg-bglight">
+          {/* <button onClick={handleSignOut}>Log Out</button> */}
+          <div>
+            <small className='text-textsecondary'>Welcome {userInfo?.name}!</small>
+            <h3>Submit a Challenge</h3>
+          </div>
+          <small className={`${errorMsg ? "block" : "hidden"} ml-1 text-destructive`}>{errorMsg}</small>
+          <div className='space-y-1'>
+            <p className='ml-1'>Type in your challenge</p>
+            <textarea
+              value={challengeInfo.challenge}
+              onChange={(e) => setChallengeInfo({ ...challengeInfo, challenge: e.target.value })}
+              required
+              placeholder="Input your challenge"
+              style={{ overflowWrap: 'break-word', wordWrap: 'break-word', wordBreak: 'break-word' }}
+              className='px-4 py-2 h-20 w-[30rem] max-w-[80vw] border border-border text-textdark rounded-lg transition ease-in-out focus:outline-accent hover:shadow-lg hover:border-accent bg-innercontainer'
+            />
+          </div>
+          <div className='flex flex-col gap-1'>
+            <p className='ml-1'>Select the type of challenge</p>
+            <DropdownMenu>
+              <DropdownMenuTrigger className='w-fit'>
+                <div
+                  className='px-4 py-2 flex flex-row gap-4 border border-border text-textdark rounded-lg transition ease-in-out focus:outline-accent hover:shadow-lg hover:border-accent bg-innercontainer'
+                >
+                  <p className={`${challengeInfo.challengeType ? "" : "text-textsecondary"}`}>
+                    {challengeInfo.challengeType ?? "Select"}
+                  </p>
+                  <ChevronDown className='stroke-textsecondary' />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem onClick={() => setChallengeInfo({ ...challengeInfo, challengeType: "Normal" })} className='focus:bg-innercontainer'>Normal</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setChallengeInfo({ ...challengeInfo, challengeType: "Daily" })} className='focus:bg-innercontainer'>Daily</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setChallengeInfo({ ...challengeInfo, challengeType: "Negative" })} className='focus:bg-innercontainer'>Negative</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <small className='text-textsecondary max-w-[25rem]'>
+              {challengeInfo.challengeType === "Normal" ? "Normal challenges will be used once each, and will go into the normal challenge pool." : ""}
+              {challengeInfo.challengeType === "Daily" ? "Daily challenges will be available every day for everyone; ex. \'Wake up at 9am\'." : ""}
+              {challengeInfo.challengeType === "Negative" ? "Negative challenges will be available every day and deduct points; ex. \'Smell bad\'." : ""}
+            </small>
+          </div>
+          <button
+            className={` ${loading ? "bg-buttondisabled" : "bg-accent hover:bg-buttonhover"} px-4 py-2 rounded-lg transition-colors cursor-pointer`}
+            onClick={handleSubmit}
+            disabled={loading}
+          >
+            Submit
+          </button>
         </div>
-        <small className={`${errorMsg ? "block" : "hidden"} ml-1 text-destructive`}>{errorMsg}</small>
-        <div className='space-y-1'>
-          <p className='ml-1'>Type in your challenge</p>
-          <textarea
-            value={challengeInfo.challenge}
-            onChange={(e) => setChallengeInfo({ ...challengeInfo, challenge: e.target.value })}
-            required
-            placeholder="Input your challenge"
-            style={{ overflowWrap: 'break-word', wordWrap: 'break-word', wordBreak: 'break-word' }}
-            className='px-4 py-2 h-20 w-[30rem] max-w-[80vw] border border-border text-textdark rounded-lg transition ease-in-out focus:outline-accent hover:shadow-lg hover:border-accent bg-innercontainer'
-          />
-        </div>
-        <div className='flex flex-col gap-1'>
-          <p className='ml-1'>Select the type of challenge</p>
-          <DropdownMenu>
-            <DropdownMenuTrigger className='w-fit'>
-              <div
-                className='px-4 py-2 flex flex-row gap-4 border border-border text-textdark rounded-lg transition ease-in-out focus:outline-accent hover:shadow-lg hover:border-accent bg-innercontainer'
-              >
-                <p className={`${challengeInfo.challengeType ? "" : "text-textsecondary"}`}>
-                  {challengeInfo.challengeType ?? "Select"}
-                </p>
-                <ChevronDown className='stroke-textsecondary' />
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem onClick={() => setChallengeInfo({ ...challengeInfo, challengeType: "Normal" })} className='focus:bg-innercontainer'>Normal</DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setChallengeInfo({ ...challengeInfo, challengeType: "Daily" })} className='focus:bg-innercontainer'>Daily</DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setChallengeInfo({ ...challengeInfo, challengeType: "Negative" })} className='focus:bg-innercontainer'>Negative</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <small className='text-textsecondary max-w-[25rem]'>
-            {challengeInfo.challengeType === "Normal" ? "Normal challenges will be used once each, and will go into the normal challenge pool." : ""}
-            {challengeInfo.challengeType === "Daily" ? "Daily challenges will be available every day for everyone; ex. \'Wake up at 9am\'." : ""}
-            {challengeInfo.challengeType === "Negative" ? "Negative challenges will be available every day and deduct points; ex. \'Smell bad\'." : ""}
-          </small>
-        </div>
-        <button
-          className={` ${loading ? "bg-buttondisabled" : "bg-accent hover:bg-buttonhover"} px-4 py-2 rounded-lg transition-colors cursor-pointer`}
-          onClick={handleSubmit}
-          disabled={loading}
-        >
-          Submit
-        </button>
       </div>
-    </div>
+    </AuthProvider>
   )
 }
 
